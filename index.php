e9795a7f0adc01eb085eb3fc0415084fae8df261
hogamarav1-max <hogamarav1@gmail.com>
2026-02-08 21:19:45 +0000

    Update index.php

--- index.php
+++ index.php
@@ -374,7 +374,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['login_key'])) {
                 }
 
                 if(data.status === "LIVE") { 
-                    setTimeout(() => { location.reload(); }, 999999999); 
+                    setTimeout(() => { location.reload(); }, 1500); 
                 }
                 
                 lines.shift();
